d7ebd5225d926bb0dcce0aa03aab0423c3dea382
mint <k022g0015@m.neec.ac.jp>
2023-04-19 01:39:39 +0900

    00_02_確認問題

--- main.cpp
+++ main.cpp
@@ -1,6 +1,6 @@
 #include <Novice.h>
 #include "Vector3.h"
-
+#include "Matrix4x4.h"
 const char kWindowTitle[] = "LD2A_カナイ_ミント";
 
 // Windowsアプリでのエントリーポイント(main関数)
@@ -13,10 +13,20 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 	char keys[256] = {0};
 	char preKeys[256] = {0};
 
-	Vector3 v1{ 1.0f,3.0f,-5.0f };
-	Vector3 v2{ 4.0f,-1.0f,2.0f };
-	float k = { 4.0f };
-	const int kRowHeight = 20;
+	Matrix4x4 m1 = {
+		3.2f,0.7f,9.6f,4.4f,
+		5.5f,1.3f,7.8f,2.1f,
+		6.9f,8.0f,2.6f,1.0f,
+		0.5f,7.2f,5.1f,3.3f
+	};
+
+	Matrix4x4 m2 = {
+		4.1f,6.5f,3.3f,2.2f,
+		8.8f,0.6f,9.9f,7.7f,
+		1.1f,5.5f,6.6f,0.0f,
+		3.3f,9.9f,8.8f,2.2f
+	};
+
 
 
 	// ウィンドウの×ボタンが押されるまでループ
@@ -31,13 +41,14 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 		///
 		/// ↓更新処理ここから
 		///
-		Vector3 resultAdd = Vector3::Add(v1, v2);
-		Vector3 resultSubtract = Vector3::Subtract(v1, v2);
-		Vector3 resultMultipry = Vector3::Multiply(k, v1);
-		float resultDot = Vector3::Dot(v1, v2);
-		float resultLength = Vector3::Length(v1);
-		Vector3 resultNormarize = Vector3::Normalize(v2);
-
+		Matrix4x4 resultAdd = Add(m1, m2);
+		Matrix4x4 resultMultipry = Multipry(m1, m2);
+		Matrix4x4 resultSubtract = Subtract(m1, m2);
+		Matrix4x4 inverseM1 = Inverse(m1);
+		Matrix4x4 inverseM2 = Inverse(m2);
+		Matrix4x4 transposeM1 = Transpose(m1);
+		Matrix4x4 transposeM2 = Transpose(m2);
+		Matrix4x4 identity = MakeIdentity4x4();
 		///
 		/// ↑更新処理ここまで
 		///
@@ -45,13 +56,15 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 		///
 		/// ↓描画処理ここから
 		///
-		VecScreenPrintf(0, 0, resultAdd, " : Add");
-		VecScreenPrintf(0, kRowHeight, resultSubtract, " : Subtract");
-		VecScreenPrintf(0, kRowHeight*2, resultMultipry, " : Multipry");
-		Novice::ScreenPrintf(0, kRowHeight * 3, "%0.2f : Dot", resultDot);
-		Novice::ScreenPrintf(0, kRowHeight * 4, "%0.2f : Length", resultLength);
-		VecScreenPrintf(0, kRowHeight * 5, resultNormarize, " : Normalize");
-
+		
+		MatrixScreenPrintf(0, 0, resultAdd,"Add");
+		MatrixScreenPrintf(0, kRowHeight * 5, resultSubtract, "Subtract");
+		MatrixScreenPrintf(0, kRowHeight*5*2, resultMultipry, "Multipry");
+		MatrixScreenPrintf(0, kRowHeight * 5*3, inverseM1, "inverseM1");
+		MatrixScreenPrintf(0, kRowHeight * 5*4, inverseM2, "inverseM2");
+		MatrixScreenPrintf(kColumnWidth * 5, 0, transposeM1, "transposeM1");
+		MatrixScreenPrintf(kColumnWidth*5, kRowHeight*5, transposeM2, "transposeM2");
+		MatrixScreenPrintf(kColumnWidth * 5, kRowHeight * 5*2, identity,"identity");
 
 
 		///
