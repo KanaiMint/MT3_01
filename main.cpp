2e1a39c64c87c29d95a0ca753dc3223710458f43
KanaiMint <k022g0015@g.neec.ac.jp>
2023-07-27 23:46:43 +0900

    04_00

--- main.cpp
+++ main.cpp
@@ -6,7 +6,7 @@
 #include<algorithm>
 const char kWindowTitle[] = "LD2A_カナイ_ミント";
 
-static float M_PI=(float)3.14159265358979323846;
+static float M_PI = (float)3.14159265358979323846;
 
 void DrawGrid(const Matrix4x4& viewProjectionMatrix, const Matrix4x4& viewportMatrix) {
 	const float kGridHalfWidth = 2.0f;//Gridの半分の幅
@@ -27,15 +27,15 @@ void DrawGrid(const Matrix4x4& viewProjectionMatrix, const Matrix4x4& viewportMa
 		Vector3 ndcMatrix2 = Transform(tmp2, viewProjectionMatrix);
 		scrMatrix2 = Transform(ndcMatrix2, viewportMatrix);
 
-		Novice::DrawLine((int)scrMatrix1.x, (int)scrMatrix1.y, (int)scrMatrix2.x, (int)scrMatrix2.y, 0xAAAAAAFF);	
+		Novice::DrawLine((int)scrMatrix1.x, (int)scrMatrix1.y, (int)scrMatrix2.x, (int)scrMatrix2.y, 0xAAAAAAFF);
 	}
 	//左から右へ線を順々に引いていく
 	for (uint32_t zIndex = 0; zIndex <= kSubdivision; ++zIndex) {
 		//ワールド座標の始点と終点
 		float z = kGridEvery * zIndex - kGridHalfWidth;
 		tmp1 = { kGridHalfWidth,0.0f,z };//始点
-		tmp2 = { - kGridHalfWidth ,0.0f,z };//終点
-		Vector3 ndcMatrix1 = Transform( tmp1, viewProjectionMatrix);
+		tmp2 = { -kGridHalfWidth ,0.0f,z };//終点
+		Vector3 ndcMatrix1 = Transform(tmp1, viewProjectionMatrix);
 		scrMatrix1 = Transform(ndcMatrix1, viewportMatrix);
 		Vector3 ndcMatrix2 = Transform(tmp2, viewProjectionMatrix);
 		scrMatrix2 = Transform(ndcMatrix2, viewportMatrix);
@@ -51,38 +51,38 @@ struct Sphere {
 };
 void DrawSphere(const Sphere& sphere, const Matrix4x4& viewProjectionMatrix, const Matrix4x4& viewportMatrix, uint32_t color) {
 	const uint32_t kSubdivision = 20;//分割数
-	const float kLonEvery = (360.0f/kSubdivision)* (M_PI / 180.0f);//経度分割一つ分の角度
-	const float kLatEvery = (360.0f / kSubdivision) *(M_PI/180.0f);//緯度分割一つ分の角度
+	const float kLonEvery = (360.0f / kSubdivision) * (M_PI / 180.0f);//経度分割一つ分の角度
+	const float kLatEvery = (360.0f / kSubdivision) * (M_PI / 180.0f);//緯度分割一つ分の角度
 
-	Vector3 ndcMatrix1 ={};
-	Vector3	scrMatrix1 ={};
-	Vector3 ndcMatrix2 ={};
-	Vector3 scrMatrix2 ={};
-	Vector3 ndcMatrix3 ={};
-	Vector3 scrMatrix3 ={};
+	Vector3 ndcMatrix1 = {};
+	Vector3	scrMatrix1 = {};
+	Vector3 ndcMatrix2 = {};
+	Vector3 scrMatrix2 = {};
+	Vector3 ndcMatrix3 = {};
+	Vector3 scrMatrix3 = {};
 	//緯度の方向に分割-pai/2~pai/2
-	for (uint32_t latIndex = 0; latIndex < kSubdivision; ++latIndex){
+	for (uint32_t latIndex = 0; latIndex < kSubdivision; ++latIndex) {
 		float lat = -M_PI / 2.0f + kLatEvery * latIndex;//現在の緯度
 		float lat_b = (M_PI / kSubdivision);
 		//経度の方向に分割θ～2pai
-		for (uint32_t lonIndex = 0; lonIndex < kSubdivision;++lonIndex) {
+		for (uint32_t lonIndex = 0; lonIndex < kSubdivision; ++lonIndex) {
 			float lon = lonIndex * kLonEvery;//現在の経度
 			float lon_b = (2 * M_PI) / kSubdivision;
 
 			//world座標系でのa,b,cを求める
 			Vector3 a, b, c;
-			a = Vector3::Add(sphere.center,Vector3::Multiply(sphere.radius,{ cosf(lat) * cosf(lon),sinf(lat),cosf(lat) * sinf(lon) }));
+			a = Vector3::Add(sphere.center, Vector3::Multiply(sphere.radius, { cosf(lat) * cosf(lon),sinf(lat),cosf(lat) * sinf(lon) }));
 			b = Vector3::Add(sphere.center, Vector3::Multiply(sphere.radius, { cosf(lat + lat_b) * cosf(lon),sinf(lat + lat_b),cosf(lat + lat_b) * sinf(lon) }));
 			c = Vector3::Add(sphere.center, Vector3::Multiply(sphere.radius, { cosf(lat) * cosf(lon + lon_b),sinf(lat),cosf(lat) * sinf(lon + lon_b) }));
 
-			 ndcMatrix1 = Transform(a, viewProjectionMatrix);
+			ndcMatrix1 = Transform(a, viewProjectionMatrix);
 			scrMatrix1 = Transform(ndcMatrix1, viewportMatrix);
 
-			 ndcMatrix2 = Transform(b, viewProjectionMatrix);
-			 scrMatrix2 = Transform(ndcMatrix2, viewportMatrix);
+			ndcMatrix2 = Transform(b, viewProjectionMatrix);
+			scrMatrix2 = Transform(ndcMatrix2, viewportMatrix);
 
-			 ndcMatrix3 = Transform(c, viewProjectionMatrix);
-			 scrMatrix3 = Transform(ndcMatrix3, viewportMatrix);
+			ndcMatrix3 = Transform(c, viewProjectionMatrix);
+			scrMatrix3 = Transform(ndcMatrix3, viewportMatrix);
 
 			Novice::DrawLine((int)scrMatrix1.x, (int)scrMatrix1.y, (int)scrMatrix2.x, (int)scrMatrix2.y, color);
 			Novice::DrawLine((int)scrMatrix1.x, (int)scrMatrix1.y, (int)scrMatrix3.x, (int)scrMatrix3.y, color);
@@ -105,15 +105,15 @@ struct Segment {
 	Vector3 origin;	//始点	
 	Vector3 diff;	//終点への差分ベクトル
 };
-Vector3 Projection(const Vector3& v1,const Vector3& v2) {
+Vector3 Projection(const Vector3& v1, const Vector3& v2) {
 	Vector3 tmp = {};
-	tmp = Vector3::Multiply(Vector3::Dot(v1, Vector3::Normalize(v2)),Vector3::Normalize(v2));
+	tmp = Vector3::Multiply(Vector3::Dot(v1, Vector3::Normalize(v2)), Vector3::Normalize(v2));
 	return tmp;
 }
 Vector3 ClossPoint(const Vector3& point, const Segment& segment) {
 	Vector3 tmp;
-	
-	tmp = Vector3::Add(segment.origin, Projection(Vector3::Subtract( point, segment.origin),segment.diff));
+
+	tmp = Vector3::Add(segment.origin, Projection(Vector3::Subtract(point, segment.origin), segment.diff));
 	//tmp = Projection(Vector3::Subtract(point,segment.origin),segment.diff);
 	return tmp;
 }
@@ -124,16 +124,16 @@ struct Plane {
 };
 
 bool Iscollision(const Sphere& sphere, const Plane& plane) {
-	
-	
+
+
 	float k = Vector3::Dot(plane.normal, sphere.center) - plane.distance;
 
-	if (std::abs(k) <=sphere.radius) {
+	if (std::abs(k) <= sphere.radius) {
 		return true;
 	}
 	return false;
 
-		//k=N・spherecenter-d;
+	//k=N・spherecenter-d;
 }
 bool IsCollision(const Line& line, const Plane& plane) {
 	//float tmp = plane.distance - (Vector3::Dot(line.origin, plane.normal));
@@ -174,7 +174,7 @@ bool IsCollision(const Segment& segment, const Plane& plane) {
 		//if (T > 0.0f) {
 		//	return true;
 		//}
-		
+
 	}
 	return false;
 }
@@ -215,10 +215,19 @@ void DrawLine(const Segment& segment, const Matrix4x4& viewProjectionMatrix, con
 	Novice::DrawLine((int)start.x, (int)start.y, (int)end.x, (int)end.y, color);
 
 }
+
+void DrawLine(Vector3& pos1, Vector3& pos2, const Matrix4x4& viewProjectionMatrix, const Matrix4x4& viewportMatrix, uint32_t color) {
+	Vector3 start = Transform(Transform(pos1, viewProjectionMatrix), viewportMatrix);
+	Vector3 end = Transform(Transform(pos2, viewProjectionMatrix), viewportMatrix);
+
+	Novice::DrawLine((int)start.x, (int)start.y, (int)end.x, (int)end.y, color);
+
+}
+
 //平面描画
 void DrawPlane(const Plane& plane, const Matrix4x4& viewProjectionMatrix, const Matrix4x4& viewportMatrix, uint32_t color) {
 
-	Vector3 center =Vector3::Multiply(plane.distance, plane.normal);//1
+	Vector3 center = Vector3::Multiply(plane.distance, plane.normal);//1
 	Vector3 perpendiculars[4];
 	perpendiculars[0] = Vector3::Normalize(Perpendicular(plane.normal));//2
 	perpendiculars[1] = { -perpendiculars[0].x,-perpendiculars[0].y,-perpendiculars[0].z };//3
@@ -227,14 +236,14 @@ void DrawPlane(const Plane& plane, const Matrix4x4& viewProjectionMatrix, const 
 	//6
 	Vector3 points[4];
 	for (int32_t index = 0; index < 4; ++index) {
-		Vector3 extend =Vector3::Multiply(2.0f, perpendiculars[index]);
+		Vector3 extend = Vector3::Multiply(2.0f, perpendiculars[index]);
 		Vector3 point = Vector3::Add(center, extend);
 		points[index] = Transform(Transform(point, viewProjectionMatrix), viewportMatrix);
 	}
 	//pointsをそれぞれ結んでDrawLineで矩形を描画する。DrawTringleを使って塗りつぶしてもいいが、DepthがないのでMT3ではわかりずらい
 	Novice::DrawLine((int)points[0].x, (int)points[0].y, (int)points[2].x, (int)points[2].y, color);
 	Novice::DrawLine((int)points[1].x, (int)points[1].y, (int)points[2].x, (int)points[2].y, color);
-	Novice::DrawLine((int)points[0].x, (int)points[0].y, (int)points[3].x, (int)points[3].y,color);
+	Novice::DrawLine((int)points[0].x, (int)points[0].y, (int)points[3].x, (int)points[3].y, color);
 	Novice::DrawLine((int)points[3].x, (int)points[3].y, (int)points[1].x, (int)points[1].y, color);
 }
 
@@ -246,9 +255,9 @@ bool IsCollision(const Triangle& triangle, const Segment& segment) {
 
 	Vector3 v01 = Vector3::Subtract(triangle.vertices[0], triangle.vertices[1]);
 	Vector3 v12 = Vector3::Subtract(triangle.vertices[1], triangle.vertices[2]);
-	Vector3 normal =Vector3::Normalize( Vector3::Cross(v01, v12));//三角形の法線
-	
-	float distance= Vector3::Dot(triangle.vertices[0], normal);
+	Vector3 normal = Vector3::Normalize(Vector3::Cross(v01, v12));//三角形の法線
+
+	float distance = Vector3::Dot(triangle.vertices[0], normal);
 
 	Plane plane(normal, distance);
 
@@ -258,11 +267,11 @@ bool IsCollision(const Triangle& triangle, const Segment& segment) {
 		float tmp2 = Vector3::Dot(plane.normal, segment.diff);
 		float T = tmp / tmp2;
 
-		Vector3 P =Vector3::Add( segment.origin ,Vector3::Multiply( T ,segment.diff));
+		Vector3 P = Vector3::Add(segment.origin, Vector3::Multiply(T, segment.diff));
 
-	//	Vector3 P = o + tb;
+		//	Vector3 P = o + tb;
 
-		//各辺を結んだベクトルと、頂点と衝突店ｐを結んだベクトルのクロス石器をとる
+			//各辺を結んだベクトルと、頂点と衝突店ｐを結んだベクトルのクロス石器をとる
 		Vector3 cross01 = Vector3::Cross(Vector3::Subtract(triangle.vertices[0], triangle.vertices[1]), Vector3::Subtract(triangle.vertices[1], P));
 		Vector3 cross12 = Vector3::Cross(Vector3::Subtract(triangle.vertices[1], triangle.vertices[2]), Vector3::Subtract(triangle.vertices[2], P));
 		Vector3 cross20 = Vector3::Cross(Vector3::Subtract(triangle.vertices[2], triangle.vertices[0]), Vector3::Subtract(triangle.vertices[0], P));
@@ -304,9 +313,9 @@ struct AABB {
 };
 
 bool IsCollision(const AABB& a, const AABB& b) {
-	if((a.min.x<=b.max.x&&a.max.x>=b.min.x)&&
+	if ((a.min.x <= b.max.x && a.max.x >= b.min.x) &&
 		(a.min.y <= b.max.y && a.max.y >= b.min.y) &&
-		(a.min.z <= b.max.z && a.max.z >= b.min.z) ){
+		(a.min.z <= b.max.z && a.max.z >= b.min.z)) {
 
 		return true;
 	}
@@ -316,7 +325,7 @@ void DrawAABB(const AABB& aabb, const Matrix4x4& viewProjectionMatrix, const Mat
 
 	Vector3 min = aabb.min;
 	Vector3 max = aabb.max;
-	 
+
 	Vector3 front[] = {
 		{min.x,min.y,min.z},//LB
 		{min.x,max.y,min.z},//LT
@@ -354,7 +363,7 @@ bool IsCollision(const AABB& aabb, const Sphere& sphere) {
 		std::clamp(sphere.center.z, aabb.min.z, aabb.max.z));
 
 	//最近接点と球の中心との距離を求める
-	float distance =Vector3::Length(Vector3::Subtract(closesPoint,sphere.center));
+	float distance = Vector3::Length(Vector3::Subtract(closesPoint, sphere.center));
 	//距離が半径より小さければ衝突
 	if (distance <= sphere.radius) {
 		return true;
@@ -391,31 +400,55 @@ void DrawBezier(const Vector3& controlPoint0, const Vector3& controlPoint1, cons
 {
 	const float t = 50;
 
-	 
-	for (int i = 1; i <=t; i++) {
+
+	for (int i = 1; i <= t; i++) {
 
 		//制御点p0,p1を線形補間
-		Vector3 p0p1 = Vector3::Lerp(controlPoint0, controlPoint1, i/t);
+		Vector3 p0p1 = Vector3::Lerp(controlPoint0, controlPoint1, i / t);
 		//制御点p1,p2を線形補間
-		Vector3 p1p2 = Vector3::Lerp(controlPoint1, controlPoint2, i/t);
+		Vector3 p1p2 = Vector3::Lerp(controlPoint1, controlPoint2, i / t);
 		//補間点p0p1,p1p2を線形補間
-		Vector3 p = Vector3::Lerp(p0p1, p1p2, i/t);
+		Vector3 p = Vector3::Lerp(p0p1, p1p2, i / t);
 
 		//制御点p0,p1を線形補間
-		Vector3 p0p1_ = Vector3::Lerp(controlPoint0, controlPoint1, (i-1)/t);
+		Vector3 p0p1_ = Vector3::Lerp(controlPoint0, controlPoint1, (i - 1) / t);
 		//制御点p1,p2を線形補間
-		Vector3 p1p2_ = Vector3::Lerp(controlPoint1, controlPoint2, (i-1)/t);
+		Vector3 p1p2_ = Vector3::Lerp(controlPoint1, controlPoint2, (i - 1) / t);
 		//補間点p0p1,p1p2を線形補間
-		Vector3 p_ = Vector3::Lerp(p0p1_, p1p2_, (i-1)/t);
-		Vector3 p_ndc= Transform(Transform(p, viewProjectionMatrix), viewportMatrix);
-		Vector3 p__ndc= Transform(Transform(p_, viewProjectionMatrix), viewportMatrix);
+		Vector3 p_ = Vector3::Lerp(p0p1_, p1p2_, (i - 1) / t);
+		Vector3 p_ndc = Transform(Transform(p, viewProjectionMatrix), viewportMatrix);
+		Vector3 p__ndc = Transform(Transform(p_, viewProjectionMatrix), viewportMatrix);
 
 		Novice::DrawLine((int)p_ndc.x, (int)p_ndc.y, (int)p__ndc.x, (int)p__ndc.y, color);
 	}
 }
 
+struct Spring {
+	//アンカー
+	Vector3 anchor;
+	float naturalLength;//自然丁
+	float stiffness;//剛性、ばね定数
+	float dampingCoefficient;//減衰係数
+};
+struct Ball {
+	Vector3 position;//ボールの位置
+	Vector3 veloctity;//ボールの速度
+	Vector3 cceleration;//ボールの加速度
+	float mass;//ボースの質量
+	float radius;//ボールの半径
+	unsigned int color;//ボールの色
+};
+
+void DrawBall(const Ball& ball, const Matrix4x4& viewProjectionMatrix, const Matrix4x4& viewportMatrix) {
+
+	Sphere sphere;
+	sphere.center = ball.position;
+	sphere.radius = ball.radius;
+
+	DrawSphere(sphere, viewProjectionMatrix, viewportMatrix, ball.color);
 
 
+}
 // Windowsアプリでのエントリーポイント(main関数)
 int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 
@@ -433,7 +466,7 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 	Segment segment
 	{
 		.origin{-0.7f,0.3f,0.0f},
-		.diff{2.0f,-0.5f,0.0f} 
+		.diff{2.0f,-0.5f,0.0f}
 	};
 	Vector3 point{ -1.5f,0.6f,0.6f };
 	/*float rot = 0.0f;*/
@@ -445,7 +478,7 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 		{-1.76f,1.0f,-0.3f},
 		{-0.94f,-0.7f,2.3f},
 	};
-	
+
 	Plane plane1{ {0.0f,1.0f,0.0f},0.1f };
 	Triangle triangle{
 		{
@@ -491,6 +524,21 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 	Matrix4x4  rotateZMatrix = MakeRotateZMatrix(rotate_.z);
 	Matrix4x4 rotateMatrix = rotateXMatrix * rotateYMatrix * rotateZMatrix;
 
+	Spring spring{};
+	spring.anchor = { 0.0f,0.0f,0.0f };
+	spring.naturalLength = 1.0f;
+	spring.stiffness = 100.0f;
+	spring.dampingCoefficient = 2.0f;
+
+	Ball ball{};
+	ball.position = { 1.2f,0.0f,0.0f };
+	ball.mass = 2.0f;
+	ball.radius = 0.05f;
+	ball.color = BLUE;
+
+	float deltaTime = 1.0f/60.0f;
+
+	
 	// ウィンドウの×ボタンが押されるまでループ
 	while (Novice::ProcessMessage() == 0) {
 		// フレームの開始
@@ -499,6 +547,10 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 		// キー入力を受け取る
 		memcpy(preKeys, keys, 256);
 		Novice::GetHitKeyStateAll(keys);
+
+
+	
+
 
 		///
 		/// ↓更新処理ここから
@@ -520,7 +572,7 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 		Sphere pointSphere{ point,0.01f };
 		Sphere clossPointSphere{closestPoint,0.01f};	*/
 
-		
+
 		Matrix4x4 worldMatrix = MakeAffineMatrix({ 1.0f,1.0f,1.0f }, rotate, translate);
 		Matrix4x4 cameraMatrix = MakeAffineMatrix({ 1.0f,1.0f,1.0f }, { 0.3f,0.0f,0.0f }, cameraTranslate);
 		Matrix4x4 viewMatrix = Inverse(cameraMatrix);
@@ -560,44 +612,46 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 		//Sphere sphere3{ {hand_tmp},0.1f };
 #pragma endregion
 
+#pragma region ばね
+		
+			Vector3 diff = ball.position - spring.anchor;
+			float length = Vector3::Length(diff);
+			if (length != 0.0f) {
+				Vector3 direction = Vector3::Normalize(diff);
+				Vector3 restPosition = spring.anchor + direction * spring.naturalLength;
+				Vector3 displacement = length * (ball.position - restPosition);
+				Vector3 restoringForce = -spring.stiffness * displacement;
+				Vector3 dampingForce = -spring.dampingCoefficient * ball.veloctity;
+				Vector3 force = restoringForce + dampingForce;
+				ball.cceleration = force / ball.mass;
+			}
+			ball.veloctity = Vector3::Add(ball.veloctity, (ball.cceleration * deltaTime));
+			ball.position = Vector3::Add(ball.position, (ball.veloctity * deltaTime));
+		
+#pragma endregion
+
+
 		ImGui::Begin("Window");
 
 		ImGui::DragFloat3("cameraTranslate", &cameraTranslate.x, 0.01f);
 		ImGui::DragFloat3("cameraRotate", &rotate.x, 0.01f);
-		//ImGui::InputFloat3("Project", &project.x, "%.3f", ImGuiInputTextFlags_ReadOnly);
-		//ImGui::DragFloat3("sphere1", &sphere1.center.x, 0.01f);
-		//ImGui::DragFloat3("sphere2", &sphere2.center.x, 0.01f);
-		//ImGui::DragFloat("Plane", &plane1.distance, 0.01f);
-		//ImGui::DragFloat3("Plane", &plane1.normal.x, 0.01f);
-		//ImGui::DragFloat3("Segment", &segment.origin.x, 0.01f);
-		//ImGui::DragFloat3("Segment", &segment.diff.x, 0.01f);
-		//ImGui::DragFloat3("Triangle", &triangle.vertices[0].x, 0.01f);
-		//ImGui::DragFloat3("AABB1", &aabb1.min.x, 0.01f);
-		//ImGui::DragFloat3("AABB1", &aabb1.max.x, 0.01f);
-		//ImGui::DragFloat3("AABB2", &aabb2.min.x, 0.01f);
-		//ImGui::DragFloat3("AABB2", &aabb2.max.x, 0.01f);
-		//ImGui::DragFloat3("Bezier_control[0]", &controlPoint[0].x, 0.01f);
-		//ImGui::DragFloat3("Bezier_control[1]", &controlPoint[1].x, 0.01f);
-		//ImGui::DragFloat3("Bezier_control[2]", &controlPoint[2].x, 0.01f);
-		/*ImGui::DragFloat3("sholder_translate", &translates[0].x, 0.01f);
-		ImGui::DragFloat3("sholder_rotate", &rotates[0].x, 0.01f);
-		ImGui::DragFloat3("elbow_translate", &translates[1].x, 0.01f);
-		ImGui::DragFloat3("elbow_rotate", &rotates[1].x, 0.01f);
-		ImGui::DragFloat3("hand_translate", &translates[2].x, 0.01f);
-		ImGui::DragFloat3("hand_rotate", &rotates[2].x, 0.01f);*/
-		ImGui::Text("c:%f,%f,%f", c.x, c.y, c.z);
-		ImGui::Text("d:%f,%f,%f", d.x, d.y, d.z);
-		ImGui::Text("e:%f,%f,%f", e.x, e.y, e.z);
-		ImGui::Text("matrix:\n%f,%f,%f,%f\n%f,%f,%f,%f\n%f,%f,%f,%f\n%f,%f,%f,%f\n",
-			rotateMatrix.m[0][0], rotateMatrix.m[0][1], rotateMatrix.m[0][2],
-			rotateMatrix.m[0][3], rotateMatrix.m[1][0], rotateMatrix.m[1][1],
-			rotateMatrix.m[1][2], rotateMatrix.m[1][3], rotateMatrix.m[2][0],
-			rotateMatrix.m[2][1], rotateMatrix.m[2][2], rotateMatrix.m[2][3],
-			rotateMatrix.m[3][0], rotateMatrix.m[3][1], rotateMatrix.m[3][2], rotateMatrix.m[3][3]
-		);
+		if (ImGui::Button("Start")) {
+		
+			spring.anchor = { 0.0f,0.0f,0.0f };
+			spring.naturalLength = 1.0f;
+			spring.stiffness = 100.0f;
+			spring.dampingCoefficient = 2.0f;
+
+			
+			ball.position = { 1.2f,0.0f,0.0f };
+			ball.mass = 2.0f;
+			ball.radius = 0.05f;
+			ball.color = BLUE;
+		}
+
 		ImGui::End();
 
-		
+
 		//2つの球の中心点間の距離を求める	
 		//float distance = sphere1.center.Length( sphere2.center );
 		//半径の合計よりも短かったら衝突	
@@ -608,7 +662,7 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 			color = WHITE;
 		}*/
 		color = WHITE;
-		
+
 		///
 		/// ↑更新処理ここまで
 		///
@@ -617,24 +671,8 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 		/// ↓描画処理ここから
 		///
 		DrawGrid(worldViewProjectionMatrix, viewportMatrix);
-		//DrawLine(segment, worldViewProjectionMatrix, viewportMatrix, color);		
-		/*DrawSphere(pointSphere, worldViewProjectionMatrix, viewportMatrix, RED);
-		DrawSphere(clossPointSphere, worldViewProjectionMatrix, viewportMatrix, BLACK);		*/
-		//DrawAABB(aabb1, worldViewProjectionMatrix, viewportMatrix, color);
-		//DrawAABB(aabb2, worldViewProjectionMatrix, viewportMatrix, color);
-		//DrawSphere(sphere1, worldViewProjectionMatrix, viewportMatrix, color);
-		//DrawSphere(sphere2, worldViewProjectionMatrix, viewportMatrix, color);
-		//DrawTriangle(triangle, worldViewProjectionMatrix, viewportMatrix, color);
-		//DrawPlane(plane1, worldViewProjectionMatrix, viewportMatrix, color);
-		
-		//DrawBezier(controlPoint[0], controlPoint[1], controlPoint[2], worldViewProjectionMatrix, viewportMatrix, color);
-		/*DrawSphere(sphere1, worldViewProjectionMatrix, viewportMatrix, BLUE);
-		DrawSphere(sphere2, worldViewProjectionMatrix, viewportMatrix, RED);
-		DrawSphere(sphere3, worldViewProjectionMatrix, viewportMatrix, GREEN);*/
-
-		/*Novice::DrawLine((int)L_NDC_sholder.x, (int)L_NDC_sholder.y, (int)L_NDC_elbow.x, (int)L_NDC_elbow.y, color);
-		Novice::DrawLine((int)L_NDC_elbow.x, (int)L_NDC_elbow.y, (int)L_NDC_hand.x, (int)L_NDC_hand.y, color);*/
-		//Novice::DrawLine(L_NDC_sholder.x, L_NDC_sholder.y, L_NDC_elbow.x, L_NDC_elbow.y, color);
+		DrawLine(ball.position, spring.anchor, worldViewProjectionMatrix, viewportMatrix, WHITE);
+		DrawBall(ball, worldViewProjectionMatrix, viewportMatrix);
 		
 		///
 		/// ↑描画処理ここまで
